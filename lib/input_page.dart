e448bb4b34a27555ba960fbeef7dc91005778697
elifogreniyor <elfogreniyor@gmail.com>
2023-06-03 01:46:34 +0300

    changed issues

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,9 +13,59 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    color: Color(0xFF1D1E33)),
+                    height: 200.0,
+                    width: 400.0,
+      ),),
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    color: Color(0xFF1D1E33)),
+                    height: 200.0,
+                    width: 170.0,
+      ),),
+              Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    color: Color(0xFF1D1E33)),
+                    height: 200.0,
+                    width: 170.0,
+      ),),
+            ],
+          )),
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    color: Color(0xFF1D1E33)),
+                    height: 200.0,
+                    width: 170.0,
+      ),),
+              Expanded(child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0), 
+                    color: Color(0xFF1D1E33)),
+                    height: 200.0,
+                    width: 170.0,
+      ),),
+            ],
+          )),
+        ],
+      )
+      
     );
   }
 }
