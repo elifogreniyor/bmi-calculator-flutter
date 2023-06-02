7e49e7c4f249148b2ac75e1826d22dea8b09d2e6
elifogreniyor <elfogreniyor@gmail.com>
2023-06-03 01:01:59 +0300

    input page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,7 +7,6 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
-    final ThemeData textTheme = ThemeData();
 
     return MaterialApp(
       theme: theme.copyWith(
@@ -15,31 +15,6 @@ class BMICalculator extends StatelessWidget {
           bodyLarge: TextStyle(color: Colors.white)),
       ),
      home: InputPage(), 
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF0F1020),
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(data: ThemeData(highlightColor: Colors.red), child: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
-      ),
     );
   }
 }
