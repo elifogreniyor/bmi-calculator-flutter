f259a6e9b66dcf934fe72117c4e55c80ff484f66
elifogreniyor <elfogreniyor@gmail.com>
2023-06-03 21:49:18 +0300

    add reusablecard class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -25,47 +25,44 @@ class _InputPageState extends State<InputPage> {
       ),),
           Expanded(child: Row(
             children: <Widget>[
+              Expanded(
+                child: new ReusableCard(colour: Color(0xFF1D1E33)),      
+      ),
               Expanded(child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0), 
-                    color: Color(0xFF1D1E33)),
-                    height: 200.0,
-                    width: 170.0,
-      ),),
-              Expanded(child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0), 
-                    color: Color(0xFF1D1E33)),
-                    height: 200.0,
-                    width: 170.0,
+                child: new ReusableCard(colour: Color(0xFF1D1E33)),   
       ),),
             ],
           )),
           Expanded(child: Row(
             children: <Widget>[
               Expanded(child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0), 
-                    color: Color(0xFF1D1E33)),
-                    height: 200.0,
-                    width: 170.0,
+                    child: new ReusableCard(colour: Color(0xFF1D1E33)),   
       ),),
               Expanded(child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0), 
-                    color: Color(0xFF1D1E33)),
-                    height: 200.0,
-                    width: 170.0,
+                    child: new ReusableCard(colour: Color(0xFF1D1E33)),   
       ),),
             ],
           )),
         ],
       )
       
+    );
+  }
+}
+class ReusableCard extends StatelessWidget {
+  
+  Color? colour;
+  
+  ReusableCard({required this.colour});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: colour,
+            borderRadius: BorderRadius.circular(10.0),
+      ),
     );
   }
 }
